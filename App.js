654d1741fa8251999a87cde78ec41c543004bab8
unknown <bgw595@myy.haaga-helia.fi>
2023-02-14 01:01:46 +0200

    first commit

--- App.js
+++ App.js
@@ -1,20 +1,65 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { useState } from 'react';
+import { Button, FlatList, StyleSheet, Text, TextInput, View, Image } from 'react-native';
 
 export default function App() {
+  const [ingridient, setIngridient] = useState('');
+  const [meals, setMeals] = useState([]);
+
+  const getMeals = () => {
+    if (ingridient !== '') {
+      fetch(`https://www.themealdb.com/api/json/v1/1/filter.php?i=${ingridient}`)
+        .then(response => response.json())
+        .then(data => setMeals(data.meals))
+        .catch(error => {
+          Alert.alert('Error', error);
+        });
+    } else {
+      alert('Please fill in the ingridient');
+    }
+  }
+
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <TextInput
+        style={styles.textfield}
+        placeholder='ingridient'
+        onChangeText={text => setIngridient(text)}
+        value={ingridient}
+      />
+      <Button title="Find" onPress={getMeals} />
+      {meals !== null && <FlatList
+        keyExtractor={(item, index) => index.toString()}
+        renderItem={({ item }) =>
+          <View>
+            <Text
+              style={{ fontSize: 18, fontWeight: "bold" }}>{item.strMeal}
+            </Text>
+            <Image
+              style={{ width: 250, height: 250 }}
+              source={{ uri: item.strMealThumb }}
+            />
+          </View>}
+        data={meals}
+      />}
+      {meals === null && <Text>There are no meals with {ingridient} ingridient</Text>}
     </View>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
+    marginTop: 60,
     flex: 1,
     backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
   },
+  textfield:
+  {
+    height: 30,
+    width: 200,
+    borderColor: 'gray',
+    borderWidth: 1
+  }
 });
